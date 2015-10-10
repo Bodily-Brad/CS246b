6afadd3edb8cea9af42a858597f2659577c19975
Bodily-Brad <bod12002@byui.edu>
2015-10-09 18:16:51 -0600

    Added "Testing..."

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello GitHub World");
+        System.out.println("Testing...");
     }
     
 }
